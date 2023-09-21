6b4cdac386f6167c01dd1f995f4a491af6c59860
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2023-09-21 20:46:32 +0200

    feat: Add typst

---
+++ lua/null-ls/builtins/formatting/typst.lua
@@ -0,0 +1,20 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+  name = "typst",
+  meta = {
+    url = "https://github.com/astrale-sharp/typstfmt",
+    description = "A typst formatter",
+  },
+  method = FORMATTING,
+  filetypes = {},
+  generator_opts = {
+    command = "$HOME/.cargo/typstfmt",
+    args = {},
+    to_stdin = true,
+  },
+  factory = h.formatter_factory,
+})
